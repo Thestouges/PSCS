97916cf75c4ac9b9a39df911dfe2593c399dde23
Thestouges <Thestouges@users.noreply.github.com>
2018-10-03 11:47:55 -0400

    Initial commit

---
+++ HomeInsurance_For_Students/src/com/cts/insurance/homequote/bo/LocationBO.java
@@ -0,0 +1,67 @@
+/**
+ * This Business Object class is used to for Location Information
+ * 
+ * @author Cognizant
+ * @contact Cognizant
+ * @version 1.0
+ */
+package com.cts.insurance.homequote.bo;
+
+import java.util.List;
+
+import com.cts.insurance.homequote.dao.LocationDAO;
+import com.cts.insurance.homequote.exception.HomequoteBusinessException;
+import com.cts.insurance.homequote.exception.HomequoteSystemException;
+import com.cts.insurance.homequote.model.Location;
+
+public class LocationBO {
+
+	/**
+	 * @param location
+	 * @return
+	 * @throws HomequoteBusinessException
+	 */
+	public int saveHomeLocation(final Location location) throws HomequoteBusinessException{
+
+		final LocationDAO locationDAO = new LocationDAO();
+		//Fill code here
+		try {
+			locationDAO.saveLocation(location);
+		} catch (HomequoteSystemException e) {
+		}
+		return 0; //return integer
+	}
+	
+	/**
+	 * @return
+	 * @throws HomequoteBusinessException
+	 */
+	public Location getHomeLocation(int quoteId) throws HomequoteBusinessException{
+
+		final LocationDAO locationDAO = new LocationDAO();
+		//Fill code here
+		try {
+			return locationDAO.getLocation(quoteId);
+		} catch (HomequoteSystemException e) {
+		}
+		
+		return null; //return Object
+	}
+	
+	/**
+	 * @return
+	 * @throws HomequoteBusinessException
+	 */
+	public List<Location> getQuoteIds(String userName) throws HomequoteBusinessException{
+
+		final LocationDAO locationDAO = new LocationDAO();
+		//Fill code here
+		try {
+			return locationDAO.getQuoteIds(userName);
+		} catch(Exception e)
+		{
+			throw new HomequoteBusinessException(e.getMessage());
+		}
+	}
+	
+}
---
+++ HomeInsurance_For_Students/src/com/cts/insurance/homequote/dao/HomeownerDAO.java
@@ -0,0 +1,88 @@
+/**
+ * This DAO class is used to for Homeowner Information
+ *
+ * @author Cognizant
+ * @contact Cognizant
+ * @version 1.0
+ */
+package com.cts.insurance.homequote.dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
+import com.cts.insurance.homequote.exception.HomequoteSystemException;
+import com.cts.insurance.homequote.model.Homeowner;
+import com.cts.insurance.homequote.util.HomeInsuranceConstants;
+import com.cts.insurance.homequote.util.SqlQueries;
+
+public class HomeownerDAO {
+	
+	private final static Logger LOG = Logger.getLogger(HomeownerDAO.class);
+
+	/**
+	 * @param homeowner
+	 */
+	public void saveHomeowner(final Homeowner homeowner) throws HomequoteSystemException
+	{
+		LOG.info("HomeownerDAO.saveHomeowner - starts");
+		Connection conn = null;
+		//Fill code here
+		PreparedStatement stmt = null;
+		try {
+			final AbstractDAOFactory daoFactory = AbstractDAOFactory.getDaoFactory(HomeInsuranceConstants.MYSQL);
+			conn = daoFactory.getConnection();
+			//INSERT INTO HomeownerInfo (QUOTE_ID, FIRST_NAME, LAST_NAME, DOB, IS_RETIRED, SSN, EMAIL_ADDRESS) VALUES (?, ?, ?, ?, ?, ?, ?)
+			stmt = conn.prepareStatement(SqlQueries.SAVE_HOMEOWNER);
+			stmt.setInt(1, homeowner.getQuoteId());
+			stmt.setString(2, homeowner.getFirstName());
+			stmt.setString(3, homeowner.getLastName());
+			stmt.setString(4, homeowner.getDob());
+			stmt.setString(5, homeowner.getIsRetired());
+			stmt.setString(6, homeowner.getSsn());
+			stmt.setString(7, homeowner.getEmailAddress());
+			stmt.executeQuery();
+			stmt.close();
+		} 
+		catch (SQLException e)
+		{
+			throw new HomequoteSystemException(e.getMessage());
+		}
+		catch(Exception e)
+		{
+			throw new HomequoteSystemException(e.getMessage());
+		}
+		finally
+		{
+			try
+			{
+				stmt.close();
+				conn.close();
+			}
+			catch (SQLException e)
+			{
+				LOG.error("Exception while trying to close Connection : " + e.getMessage() );
+			}
+		}
+	}
+	
+
+	/**
+	 * @param homeowner
+	 */
+	public Homeowner getHomeowner(final int quoteId) throws HomequoteSystemException
+	{
+		LOG.info("HomeownerDAO.getHomeowner - starts");
+		Connection conn = null;
+		Homeowner homeowner = null;
+		ResultSet resultSet = null;
+		PreparedStatement stmt = null;
+		//Fill code here
+		return null; //return Object
+	}
+
+}
